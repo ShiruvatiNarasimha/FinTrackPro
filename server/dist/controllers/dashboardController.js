992857a4b9c06e0538c8b178b87d9e317eb876b0
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-10-11 21:11:12 +0530

    updated code

--- server/dist/controllers/dashboardController.js
+++ server/dist/controllers/dashboardController.js
@@ -1,50 +1,44 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getDashboardMetrics = void 0;
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
-const getDashboardMetrics = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const getDashboardMetrics = async (req, res) => {
     try {
-        const popularProducts = yield prisma.products.findMany({
+        const popularProducts = await prisma.products.findMany({
             take: 15,
             orderBy: {
                 stockQuantity: "desc",
             },
         });
-        const salesSummary = yield prisma.salesSummary.findMany({
+        const salesSummary = await prisma.salesSummary.findMany({
             take: 5,
             orderBy: {
                 date: "desc",
             },
         });
-        const purchaseSummary = yield prisma.purchaseSummary.findMany({
+        const purchaseSummary = await prisma.purchaseSummary.findMany({
             take: 5,
             orderBy: {
                 date: "desc",
             },
         });
-        const expenseSummary = yield prisma.expenseSummary.findMany({
+        const expenseSummary = await prisma.expenseSummary.findMany({
             take: 5,
             orderBy: {
                 date: "desc",
             },
         });
-        const expenseByCategorySummaryRaw = yield prisma.expenseByCategory.findMany({
+        const expenseByCategorySummaryRaw = await prisma.expenseByCategory.findMany({
             take: 5,
             orderBy: {
                 date: "desc",
             },
         });
-        const expenseByCategorySummary = yield expenseByCategorySummaryRaw.map((item) => (Object.assign(Object.assign({}, item), { amount: item.amount.toString() })));
+        const expenseByCategorySummary = await expenseByCategorySummaryRaw.map((item) => ({
+            ...item,
+            amount: item.amount.toString(),
+        }));
         res.json({
             popularProducts,
             salesSummary,
@@ -56,5 +50,5 @@ const getDashboardMetrics = (req, res) => __awaiter(void 0, void 0, void 0, func
     catch (error) {
         res.status(500).json({ message: "Error retrieving dashboard metrics" });
     }
-});
+};
 exports.getDashboardMetrics = getDashboardMetrics;
