9452c8649e687cbc014b3714d6065cda18d39f2c
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-10-11 10:26:40 +0530

    Added Settings page

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import morgan from "morgan";
 
 import dashboardRoutes from "./routes/dashboardRoutes";
 import productRoutes from "./routes/productRoutes";
+import userRoutes from "./routes/userRoutes";
 
 dotenv.config();
 const app = express();
@@ -20,6 +21,7 @@ app.use(cors());
 
 app.use("/dashboard", dashboardRoutes);
 app.use("/products", productRoutes);
+app.use("/users", userRoutes);
 
 const port = Number(process.env.PORT) || 3001;
 app.listen(port, "0.0.0.0", () => {
